73ef610a58148c6221bdf7c99badf703b3b8e2fe
snuffy <72365477+snuffyDev@users.noreply.github.com>
2022-12-22 19:42:45 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod hls;
 use async_recursion::async_recursion;
 use hls::body::modify_hls_body;
 
+use hyper::client::HttpConnector;
 use hyper::header::{self};
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::{Body, Request};
 use hyper::{Client, Method, Response, StatusCode};
 use hyper_tls::HttpsConnector;
+use lazy_static::lazy_static;
 use std::net::SocketAddr;
 
 use tokio::net::TcpListener;
@@ -35,24 +37,27 @@ enum ResponseResult {
     Ok(Response<Body>),
     Err(String),
 }
-// Sends a HTTP request
-#[async_recursion]
-async fn send_request(url: &str, host: &str) -> ResponseResult {
-    let https = HttpsConnector::new();
-    let req_url = &url;
 
-    let client = Client::builder()
+lazy_static! {
+    static ref HTTPS: HttpsConnector<HttpConnector> = HttpsConnector::new();
+    static ref HTTP_CLIENT: Client<HttpsConnector<HttpConnector>> = Client::builder()
         .pool_max_idle_per_host(0)
         .pool_idle_timeout(std::time::Duration::from_millis(0))
         .pool_max_idle_per_host(0)
         .http1_title_case_headers(true)
         .http1_preserve_header_case(true)
         .http2_keep_alive_timeout(Duration::new(20, 0))
-        .build::<_, Body>(https);
+        .build::<_, Body>(HTTPS.to_owned());
+}
+
+// Sends a HTTP request
+#[async_recursion]
+async fn send_request(url: &str, host: &str) -> ResponseResult {
+    let req_url = &url;
 
     let req = Request::builder()
-        .uri(req_url.parse::<hyper::Uri>().expect("Error parsing Uri"))
-        .method("GET")
+        .uri(req_url.to_string())
+        .method("GET").header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
         .header("Origin", format!("https://{}", &host))
         .body(Body::empty());
 
@@ -61,7 +66,7 @@ async fn send_request(url: &str, host: &str) -> ResponseResult {
         Err(e) => return ResponseResult::Err(e.to_string()),
     };
 
-    match client.request(request).await {
+    match HTTP_CLIENT.request(request).await {
         Ok(res) => {
             if let Some(location_header) = res.headers().get(header::LOCATION) {
                 return send_request(&location_header.to_str().unwrap(), host).await;
@@ -80,9 +85,8 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
         "*".parse::<hyper::http::HeaderValue>().unwrap(),
     );
 
-    let path = req.uri().path().to_string(); // Split the URL Path by "/", and returns each str slice
-    let first_path_part = path.split("/").nth(1).unwrap_or("");
-
+    let path = req.uri().path().to_string();
+    let first_path_part = path.splitn(3, "/").nth(1).unwrap_or(""); // Split the URL Path by "/", up to 3 "/"'s, return index 1
     let query = req.uri().query().unwrap_or("host=none");
 
     // Collect all the URL Search Params into a HashMap
@@ -91,29 +95,27 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
         .collect::<HashMap<String, String>>();
 
     // Get the URL Search Param `&host=`
-    let host = query_map.get("host").unwrap().as_str();
+    let host = query_map.get("host").unwrap();
 
     // Matches Request Method and the first URL Path section
     match (req.method(), first_path_part) {
         (&Method::GET, "videoplayback") => {
-            if &host == &"none" {
-                return Ok(response);
-            }
             let url = format!("https://{}{}?{}", &host, &path, &query);
 
-            let result = match send_request(&url, &host).await {
-                ResponseResult::Ok(r) => r,
-                ResponseResult::Err(e) => Response::builder().status(500).body(e.into()).unwrap(),
-            };
-
-            *response.body_mut() = result.into_body();
-
-            Ok(response)
+            match send_request(&url, &host).await {
+                ResponseResult::Ok(mut r) => {
+                    r.headers_mut().insert(
+                        hyper::header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                        "*".parse::<hyper::http::HeaderValue>().unwrap(),
+                    );
+                    Ok(r)
+                }
+                ResponseResult::Err(e) => {
+                    Ok(Response::builder().status(500).body(e.into()).unwrap())
+                }
+            }
         }
         (&Method::GET, "api") => {
-            if &host == &"none" {
-                return Ok(response);
-            }
             let url = format!("https://manifest.googlevideo.com{}", &path);
 
             let result = match send_request(&url, &host).await {
@@ -126,10 +128,9 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
                 .await
                 .unwrap_or_default()
                 .to_vec();
-            let body_str = String::from_utf8(body).unwrap();
-
+            let body_str = unsafe { String::from_utf8_unchecked(body) };
             // Modify the HLS Manifest body
-            let result = modify_hls_body(body_str, host)
+            let result = modify_hls_body(body_str, &host)
                 .await
                 .expect("Could not modify HLS body.");
 
